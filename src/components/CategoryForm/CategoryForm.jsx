a718218817649dedc06a6dc45be93552967901b1
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-09-15 23:50:16 +0530

    changed save button color from btn-primary to btn-warning

--- src/components/CategoryForm/CategoryForm.jsx
+++ src/components/CategoryForm/CategoryForm.jsx
@@ -40,7 +40,7 @@ const CategoryForm = () => {
                                         placeholder="#ffffff"
                                 />
                             </div>
-                            <button type="submit" className="btn btn-primary w-100" >Save</button>
+                            <button type="submit" className="btn btn-warning w-100" >Save</button>
 
                         </form>
                     </div>
