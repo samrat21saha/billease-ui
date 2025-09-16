632d1841c244f67eb339f7feb2fb40879dd85289
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-09-16 22:12:19 +0530

    feat: add Explore layout with categories, items, customer form, cart items, and summary sections

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,6 +1,47 @@
 const UserForm = () => {
     return (
-        <div>User Form</div>
+    <div className="mx-2 mt-2">
+            <div className="row">
+                <div className="card col-md-8 form-container">
+                    <div className="card-body">
+                        <form>
+                            
+                            <div className="mb-3">
+                                <label htmlFor="name" className="form-label">Name</label>
+                                <input  type="text"
+                                        name="name"
+                                        id="name"
+                                        className="form-control"
+                                        placeholder="John Doe" 
+                                />
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="name" className="form-label">Email</label>
+                                <input  type="email"
+                                        name="email"
+                                        id="email"
+                                        className="form-control"
+                                        placeholder="johndoe@example.com" 
+                                />
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="name" className="form-label">Password</label>
+                                <input  type="password"
+                                        name="password"
+                                        id="password"
+                                        className="form-control"
+                                        placeholder="**********" 
+                                />
+                            </div>
+                            <button type="submit" className="btn btn-warning w-100" >Save</button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default UserForm;
