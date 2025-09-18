2d6cb170d7e1ebb6d17df510d86005dca445fe11
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-09-18 20:32:04 +0530

    feat: add initial CategoryForm.jsx with state management for inputs and image

--- src/components/CategoryForm/CategoryForm.jsx
+++ src/components/CategoryForm/CategoryForm.jsx
@@ -1,23 +1,49 @@
+import { useState } from "react";
+import { assets } from "../../assets/assets";
+
 const CategoryForm = () => {
+
+    const [loading, setLoading] = useState(false);
+    const [image, setImage] = useState(false);
+    const [data, setData] = useState({
+        name: '',
+        description: '',
+        bgColor: '#ffffff'
+    });
+
+
+    const onChangeHandler = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+        setData((data) => ({
+            ...data,
+            [name]: value
+        }));
+    }
+
+
     return (
         <div className="mx-2 mt-2">
             <div className="row">
-                <div className="card col-md-8 form-container">
+                <div className="card col-md-12 form-container">
                     <div className="card-body">
                         <form>
                             <div className="mb-3">
                                 <label htmlFor="image" className="form-label">
-                                    <img src="https://placehold.co/48x48" alt="" width={48} />
+                                    <img src={image ? URL.createObjectURL(image) : assets.upload} alt="" width={48} />
                                 </label>
-                                <input type="file" name="image" id="image" className="form-control" hidden/>
+                                <input type="file" name="image" id="image" className="form-control" hidden onChange={() => setImage(e.target.files[0])}/>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="name" className="form-label">Name</label>
                                 <input  type="text"
-                                        name="name"
-                                        id="name"
-                                        className="form-control"
-                                        placeholder="Category Name" 
+                                    name="name"
+                                    id="name"
+                                    className="form-control"
+                                    placeholder="Category Name" 
+
+                                    onChange={onChangeHandler}
+                                    value={data.name}
                                 />
                             </div>
                             <div className="mb-3">
@@ -27,7 +53,11 @@ const CategoryForm = () => {
                                         name="name"
                                         id="description"
                                         className="form-control"
-                                        placeholder="Category Description"></textarea>
+                                        placeholder="Category Description"
+                                        
+                                        onChange={onChangeHandler}
+                                        value={data.description}
+                                ></textarea>
                                 
                             </div>
 
@@ -37,6 +67,8 @@ const CategoryForm = () => {
                                 <input  type="color"
                                         name="bgcolor"
                                         id="bgcolor"
+                                        onChange={onChangeHandler}
+                                        value={data.bgColor}
                                         placeholder="#ffffff"
                                 />
                             </div>
