21ab06483a9fb1ed20a65953aadfbbe6089a42a6
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-10-07 22:29:50 +0530

    feat: extend AppContext with itemsData and auth state management using localStorage sync

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,21 +1,41 @@
 import { createContext, useEffect, useState } from "react";
+import { fetchCategories } from "../Service/CategoryService";
 
 export const AppContext = createContext(null);
 export const AppContextProvider = (props) => {
 
     const [categories, setCategories] = useState([]);
+    const [itemsData, setItemsData] = useState([]);
+    const [autn, setAuth] = useState({token: null, role: null});
 
     useEffect(() => {
         async function loadData() {
+            if (localStorage.getItem("token") && localStorage.getItem("role")) { 
+                setAuthData(
+                    localStorage.getItem("token"),
+                    localStorage.getItem("role")
+                );
+            }
             const response = await fetchCategories();
+            const itemResponse = await fetchItems();
             setCategories(response.data);
+            setItemsData(itemResponse.data);
         }
         loadData();
     }, []);
 
+
+    const setAuthData = (token, role) => {
+        setAuth({token, role});
+    }
+
     const contextValue = {
         categories,
-        setCategories
+        setCategories,
+        auth,
+        setAuthData,
+        itemsData,
+        setItemsData
     };
 
     return <AppContext.Provider value={contextValue}>   
--- src/pages/ManageItems/ManageItems.jsx
+++ src/pages/ManageItems/ManageItems.jsx
@@ -1,3 +1,5 @@
+import ItemForm from '../../components/ItemForm/ItemForm';
+import ItemList from '../../components/ItemList/ItemList';
 import './ManageItems.css';
 const ManageItems = () => {
 
@@ -5,10 +7,10 @@ const ManageItems = () => {
     <div className="items-container text-light">
 
             <div className="left-column">
-                items form
+                <ItemForm />
             </div>
             <div className="right-column">
-                list of items
+                <ItemList/>
             </div>
         </div>
     )
