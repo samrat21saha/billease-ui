6d64cda372636bb98c775b99b413d751b27bbd24
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-10-17 23:10:04 +0530

    added itemName, itemPrice, itemImage, itemId

--- src/components/DisplayItems/DisplayItems.jsx
+++ src/components/DisplayItems/DisplayItems.jsx
@@ -1,13 +1,23 @@
 import { use, useContext } from 'react';
 import './DisplayItems.css';
 
-const DisplayItems = () => {  
-    const {itemsData} = useContext(AppContext);
+const DisplayItems = () => {
+    const { itemsData } = useContext(AppContext);
     return (
-        <div className="row g-3">
-            {itemsData.map((item, index) => (
-                <div key={index} className=''> </div>
+        <div className="p-3">
+            <div className="row g-3">
+                {itemsData.map((item, index) => (
+                    <div key={index} className="col-md-4 col-sm-6">
+                        <item
+                            itemName={item.name}
+                            itemPrice={item.price}
+                            itemImage={item.imgUrl}
+                            itemId={item.itemId}
+                        />
+                    </div>
+                ))}
 
+            </div>
         </div>
     )
 }
